a304b6dca8a913da1c7970923bcc5231e919ae12
venh <conf312@naver.com>
2024-07-15 16:53:17 +0900

    Update RemoveDuplicates.java

--- concept/basic-coding/배열에서 중복 제거하기/RemoveDuplicates.java
+++ concept/basic-coding/배열에서 중복 제거하기/RemoveDuplicates.java
@@ -1,5 +1,9 @@
 import java.util.*;
-
+/*
+1. Set 자료구조를 사용하여 중복을 자동으로 제거합니다.
+2. Set에 모든 요소를 추가한 후, 다시 배열로 변환합니다.
+Set을 이용한 방법은 중복된 요소를 자동으로 제거하며, 평균 시간 복잡도가 O(n)입니다. 하지만 순서가 보장되지 않으므로, 순서가 필요한 경우 LinkedHashSet을 사용할 수 있습니다.
+*/
 public class RemoveDuplicates {
     public static int[] removeDuplicates(int[] nums) {
         Set<Integer> uniqueSet = new HashSet<>();
